c3852120722e3244010ef176f437abd57b7b8805
gayathriveera-stack <gayathriveera@gmail.com>
2026-04-02 15:39:00 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -180,8 +180,7 @@ if (/job\s*\|/i.test(subject)) return null;
 // ❌ Ignore USYD mails
 if (/Admissions\s*/i.test(subject)) return null;
   if (/emba\s*&\s*mba\s*alumni\s*invitation/i.test(subject)) return null;
-
-  
+if (/Admission\s*/i.test(subject)) return null;
   
   // ✅ Accept if job-like
   const isJobEmail =
